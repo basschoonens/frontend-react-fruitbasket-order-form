80381b2f7a830826ebe6520a1276fc24c3150179
basschoonens <bas.schoonens@gmail.com>
2024-02-15 21:41:08 +0100

    form aangemaakt. styling gedaan. alle formdata moet nog verzameld en weergegeven in de log na verzenden.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,21 @@ function App() {
     const [appels, setAppels] = useState(0)
     const [kiwis, setKiwis] = useState(0)
 
+    const [formState, setFormState] = useState({
+        firstname: '',
+        lastname: '',
+        age: '',
+        postalCode: '',
+        deliveryFrequency: '',
+        comments: '',
+        terms: false
+    });
+
+    function handleChange(e) {
+        setFormState({
+            firstname: e.target.value
+        })
+    }
 
     const resetCounters = () => {
         setAardbeien(0)
@@ -20,9 +35,7 @@ function App() {
     return (
         <>
             <main className="page-container">
-                <span>
-                <img className="logo-image" src={logo} alt="logo"/>
-                </span>
+                <span><img className="logo-image" src={logo} alt="logo"/></span>
                 <h1>Fruitmand bezorgservice</h1>
                 <article className="fruit-container">
                     <h2>Aardbeien </h2>
@@ -31,7 +44,9 @@ function App() {
                                 onClick={() => setAardbeien(previous => previous - 1)}>-
                         </button>
                         <p>{aardbeien}</p>
-                        <button className="plusButton, button" onClick={() => setAardbeien(previous => previous + 1)}>+</button>
+                        <button className="plusButton, button"
+                                onClick={() => setAardbeien(previous => previous + 1)}>+
+                        </button>
                     </div>
                 </article>
                 <article className="fruit-container">
@@ -41,7 +56,8 @@ function App() {
                                 onClick={() => setBananen(previous => previous - 1)}>-
                         </button>
                         <p>{bananen}</p>
-                        <button className="plusButton, button" onClick={() => setBananen(previous => previous + 1)}>+</button>
+                        <button className="plusButton, button" onClick={() => setBananen(previous => previous + 1)}>+
+                        </button>
                     </div>
                 </article>
                 <article className="fruit-container">
@@ -51,7 +67,8 @@ function App() {
                                 onClick={() => setAppels(previous => previous - 1)}>-
                         </button>
                         <p>{appels}</p>
-                        <button className="plusButton, button" onClick={() => setAppels(previous => previous + 1)}>+</button>
+                        <button className="plusButton, button" onClick={() => setAppels(previous => previous + 1)}>+
+                        </button>
                     </div>
                 </article>
                 <article className="fruit-container">
@@ -61,11 +78,26 @@ function App() {
                                 onClick={() => setKiwis(previous => previous - 1)}>-
                         </button>
                         <p>{kiwis}</p>
-                        <button className="plusButton, button" onClick={() => setKiwis(previous => previous + 1)}>+</button>
+                        <button className="plusButton, button" onClick={() => setKiwis(previous => previous + 1)}>+
+                        </button>
                     </div>
                 </article>
-
-                <button className="resetCounters" type="button" onClick={() => resetCounters()}>Reset</button>
+                <button className="btn-reset-counters" type="button" onClick={() => resetCounters()}>Reset</button>
+                <form className="order-form">
+                    <label htmlFor="firstname">Voornaam : <input type="text" name="firstname"/></label>
+                    <label htmlFor="lastname">Achternaam : <input type="text" name="lastname"/></label>
+                    <label htmlFor="age">Leeftijd : <input type="number"/></label>
+                    <label htmlFor="postal-code">Postcode : <input type="text"/></label>
+                    <label htmlFor="delivery-frequency">Bezorgfrequentie : </label>
+                    <select>
+                        <option>iedere week</option>
+                        <option>om de week</option>
+                        <option>iedere maand</option>
+                    </select>
+                    <label htmlFor="comments">Opmerkingen</label><textarea/>
+                    <label htmlFor="terms"><input type="checkbox"/> Ik ga akkoord met de voorwaarden</label>
+                    <button className="btn-submit-form" type="submit">Verstuur</button>
+                </form>
             </main>
         </>
     )
